4320c5969bd52dfe10099065edc88aaaf8d56d96
lixiang007666 <88304454@qq.com>
2022-09-30 03:10:08 +0000

    Add BatchNormal layer

--- idealflow/core/layer.py
+++ idealflow/core/layer.py
@@ -556,3 +556,72 @@ class LSTM(Layer):
     @property
     def param_names(self):
         return "W_g", "b_g", "W_c", "b_c"
+
+class BatchNormalization(Layer):
+
+    def __init__(self,
+                 momentum=0.99,
+                 gamma_init=Ones(),
+                 beta_init=Zeros(),
+                 epsilon=1e-5):
+        super().__init__()
+        self.m = momentum
+        self.epsilon = epsilon
+        # gamma 扩展参数
+        # beta 平移参数
+        self.initializers = {"gamma": Ones(), "beta": Zeros()}
+        self.reduce = None
+
+    def forward(self, inputs):
+        # self.reduce = (0,) if inputs.ndim == 2 else (0, 1, 2)
+        self.reduce = (0,)
+        if not self.is_init:
+            for p in self.param_names:
+                self.shapes[p] = inputs.shape[-1]
+            self._init_params()
+
+        if self.nt_params["r_mean"] is None:
+            self.nt_params["r_mean"] = inputs.mean(self.reduce, keepdims=True)
+            self.nt_params["r_var"] = inputs.var(self.reduce, keepdims=True)
+
+        if self.is_training:
+            mean = inputs.mean(self.reduce, keepdims=True)
+            var = inputs.var(self.reduce, keepdims=True)
+            self.nt_params["r_mean"] = (self.m * self.nt_params["r_mean"] +
+                                        (1.0 - self.m) * mean)
+            self.nt_params["r_var"] = (self.m * self.nt_params["r_var"] +
+                                       (1.0 - self.m) * var)
+        else:
+            mean = self.nt_params["r_mean"]
+            var = self.nt_params["r_var"]
+
+        # standardize
+        X_center = inputs - mean
+        std = (var + self.epsilon) ** 0.5
+        X_norm = X_center / std
+        self.ctx = {"X_norm": X_norm, "std": std, "X_center": X_center}
+        return self.params["gamma"] * X_norm + self.params["beta"]
+
+    def backward(self, grad):
+        # grads w.r.t. params
+        self.grads["gamma"] = (self.ctx["X_norm"] * grad).sum(self.reduce)
+        self.grads["beta"] = grad.sum(self.reduce)
+
+        # N = grad.shape[0]
+        N = np.prod([grad.shape[d] for d in self.reduce])
+        std_inv = 1.0 / self.ctx["std"]
+        # grads w.r.t. inputs
+        # ref: http://cthorey.github.io./backpropagation/
+        d_in = (1.0 / N) * self.params["gamma"] * std_inv * (
+            N * grad - np.sum(grad, axis=self.reduce, keepdims=True) -
+            self.ctx["X_center"] * std_inv ** 2 *
+            (grad * self.ctx["X_center"]).sum(axis=self.reduce, keepdims=True))
+        return d_in
+
+    @property
+    def param_names(self):
+        return "gamma", "beta"
+
+    @property
+    def nt_param_names(self):
+        return "r_mean", "r_var"
---
+++ test/layer/test_bn.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pytest
+import idealflow as iflow
+
+def test_batch_normalization():
+    input_ = np.array([[1.0, 2.0, 3.0, 4.0, 5.0],
+                       [5.0, 4.0, 3.0, 2.0, 1.0]])
+    mom, epsilon = 0.9, 1e-5
+    layer = tn.layer.BatchNormalization(momentum=mom, epsilon=epsilon)
+    for i in range(3):
+        layer.forward(input_)
+        mean = input_.mean(0, keepdims=True)
+        var = input_.var(0, keepdims=True)
+        if i == 0:
+            r_mean = mean
+            r_var = var
+        else:
+            r_mean = mom * r_mean + (1 - mom) * mean
+            r_var = mom * r_var + (1 - mom) * var
+        assert np.allclose(layer.ctx["X_norm"],
+                           (input_ - mean) / (var + epsilon) ** 0.5)
+
+    layer.is_training = False
+    layer.forward(input_)
+    assert np.allclose(layer.ctx["X_norm"],
+                       (input_ - r_mean) / (r_var + epsilon) ** 0.5)
+
+if __name__=="__main__":
+    test_batch_normalization()
