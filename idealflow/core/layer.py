ecea13ea8d6dea4d26730bf282fd99d00e68df3c
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2022-10-03 12:26:48 +0800

    Update layer.py

--- idealflow/core/layer.py
+++ idealflow/core/layer.py
@@ -645,3 +645,24 @@ class Flatten(Reshape):
 
     def __init__(self):
         super().__init__(-1)
+        
+class Dropout(Layer):
+
+    def __init__(self, keep_prob=0.5):
+        super().__init__()
+        self._keep_prob = keep_prob
+        self._multiplier = None
+
+    def forward(self, inputs):
+        if self.is_training:
+            multiplier = np.random.binomial(1, self._keep_prob, inputs.shape)
+            self._multiplier = multiplier / self._keep_prob
+            outputs = inputs * self._multiplier
+        else:
+            outputs = inputs
+        return outputs
+
+    def backward(self, grad):
+        assert self.is_training is True
+        return grad * self._multiplier
+ 
