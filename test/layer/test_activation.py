a4e6500598311b26a94323299864f41a18d9dc2c
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2022-10-04 11:16:09 +0800

    Create test_activation.py

---
+++ test/layer/test_activation.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+import idealflow as flow
+
+@pytest.mark.parametrize("activation_layer, expect_range",
+                         [(iflow.layer.Sigmoid(), (0, 1)),
+                          (iflow.layer.Tanh(), (-1, 1)),
+                          (iflow.layer.ReLU(), (0, np.inf)),
+                          (iflow.layer.LeakyReLU(), (-np.inf, np.inf)),
+                          (iflow.layer.Softplus(), (0, np.inf))])
+def test_activation(activation_layer, expect_range):
+    """Test expected output range of activation layers"""
+    input_ = np.random.normal(size=(100, 5))
+    net = iflow.net.Net([tn.layer.Dense(1), activation_layer])
+    output = net.forward(input_)
+    lower_bound, upper_bound = expect_range
+    assert np.all((output >= lower_bound) & (output <= upper_bound))
+  
